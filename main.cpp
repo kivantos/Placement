52774bc949947f8f0c57f77df97476735c8b496a
Rasmus Schroeder <supernonsense@web.de>
2013-06-21 14:28:41 +0200

    Alle Tabs entfernt

--- main.cpp
+++ main.cpp
@@ -22,14 +22,14 @@ int main(int argc, char **argv)
       cout << "Programme call: ./placement <INSTANCE>" << endl;
       return 1;
    }
-   
+
    vector<int> test;
 
    Instance Inst;
    Inst.read_file(argv[1]);
    Inst.minimum_perimeter();
-   
+
    Inst.print();
    return 0;
-   
+
 }
